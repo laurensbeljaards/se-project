ba0edb7f62c34b7e8b0df2f18c38c3f7d65d7d63
Pascal <p.l.j.roeleven@umail.leidenuniv.nl>
2015-11-06 13:14:11 +0100

    Feedback by Daniel

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once($BASEDIR . 'config.php');
+require_once('config.php');
 
 include $BASEDIR . 'header/header.php';
 
@@ -29,7 +29,8 @@ $templateCode = $templateCode["templatecode"];
 //check if user already saved this assignment
 $sql = "SELECT * FROM `student_opdracht` WHERE `student_id` = " . $studentid . " AND `opdracht_id` = " . $opdrid . ";";
 $alreadySaved = $conn->query($sql);
-
+$sql = "SELECT layout, werkt, testdata, overig FROM Feedback WHERE student_id=". $studentid . " AND opdracht_id=" . $opdrid;		
+$feedbackResult = $conn->query($sql);
 if (!$alreadySaved) {
 	echo $conn->error;
 }
@@ -81,8 +82,47 @@ if($allRequirements->num_rows > 0){
 
 
 <div class="extrainfoheader">
-	<div style='margin-top: 40px;'>&nbsp</div>
+	<!--<div style='margin-top: 40px;'>&nbsp </div>-->
+		<div class="extrainfosub">
+			<h5>Feedback:</h5>
+		</div>
+				<?php
+					// checkt eerst of er feedback is voor de opdracht, als dit het geval is dan print het de feedback op het scherm.
+					if ($feedbackResult->num_rows > 0) {
+						echo "<h5>Goed</h5>";
+						$row = $feedbackResult->fetch_assoc();
+						if($row["layout"]){
+							echo "Layout<br>";
+						}
+						if($row["werkt"]){
+							echo "Compileren<br>";
 
+						}
+						if($row["testdata"]){
+							echo "Basis requirements<br>";
+						}
+						if($row["overig"]){
+							echo "Extra requirements<br>";
+						}
+						echo "<h5>Niet goed</h5>";
+						if(!$row["layout"]){
+							echo "Layout<br>";
+						}
+						if(!$row["werkt"]){
+							echo "Compileren<br>";
+
+						}
+						if(!$row["testdata"]){
+							echo "Basis requirements<br>";
+						}
+						if(!$row["overig"]){
+							echo "Extra requirements<br>";
+						}
+					}
+					else{
+						echo "Je hebt voor deze opdracht nog geen feedback ontvangen.";
+					}
+		?>
 	<div class="extrainfosub">
 		<h5>Available Tasks:</h5>
     </div>
@@ -91,7 +131,7 @@ if($allRequirements->num_rows > 0){
         if($allAssignments->num_rows > 0){
             while($row = $allAssignments->fetch_assoc()){
                 echo '<a href ="index.php?opdr='.$row["id"].'"><li id ="' . $row["id"] . '">'. $row["naam"] . '</li></a>';
-                echo '<div style="width: 260px" id ="hidden' . $row["id"]. '">' . $row["description"] . '</div>'; 
+                echo '<div style="width: 240px" id ="hidden' . $row["id"]. '">' . $row["description"] . '</div>'; 
                 
                  ?>
                  <script type="text/javascript">
@@ -173,84 +213,84 @@ if($allRequirements->num_rows > 0){
 </div>
 
 <script>
-                document.querySelector("textarea").addEventListener
-                ('keypress',function(keystroke) {//ignores special keys like shift and tab (eg shift + ] is }, but } will then not be recognised)
-                    if (keystroke.keyCode === 13) {//The keystroke was an [enter]
-                        var start = this.selectionStart;
-                        var target = keystroke.target;
-                        var value = target.value;
-                        var newline ="";
-                                                                    
-                        var brackets = 0;
-                        //Goes back to the beginning of the line and checks for tabs
-                        for (var i = textarea.selectionStart-1; i >= 0 ; i--) {
-                            if (value[i] == "\t") {//Tabs for each tab at the beginning of the previous line
-                                newline = newline + "\t";
-                            } else if (value[i] == "\n") {//Beginning of the line
-                                break;
-                            } else if (value[i] == "{") {
-                                brackets ++;
-                            } else if (value[i] == "}" && brackets <= 0) {//No additional tab if as many brackets on this line were closed as opened
-                                brackets --;
-                            } else {//Only add tabs if there's no other characters before them
-                                newline = "";
-                            }
-                        }
-                        if (brackets > 0)//add one additional tab
-                            newline += "\t";
-                        
-                        //The textarea value becomes: text before cursor + [enter] + an amount of tabs + text after cursor
-                        target.value = value.substring(0, start) + "\n" + newline + value.substring(this.selectionEnd);
-                        //put cursor after the old position + 1 enter + all tabs.
-                        this.selectionStart = this.selectionEnd = start + newline.length + 1;
-                        keystroke.preventDefault();
-                        
-                    } else if (keystroke.keyCode === 125) {// "}"
-                 
-                        var start = this.selectionStart;
-                        var target = keystroke.target;
-                        var value = target.value;
+	document.querySelector("textarea").addEventListener
+	('keypress',function(keystroke) {//ignores special keys like shift and tab (eg shift + ] is }, but } will then not be recognised)
+		if (keystroke.keyCode === 13) {//The keystroke was an [enter]
+			var start = this.selectionStart;
+			var target = keystroke.target;
+			var value = target.value;
+			var newline ="";
+														
+			var brackets = 0;
+			//Goes back to the beginning of the line and checks for tabs
+			for (var i = textarea.selectionStart-1; i >= 0 ; i--) {
+				if (value[i] == "\t") {//Tabs for each tab at the beginning of the previous line
+					newline = newline + "\t";
+				} else if (value[i] == "\n") {//Beginning of the line
+					break;
+				} else if (value[i] == "{") {
+					brackets ++;
+				} else if (value[i] == "}" && brackets <= 0) {//No additional tab if as many brackets on this line were closed as opened
+					brackets --;
+				} else {//Only add tabs if there's no other characters before them
+					newline = "";
+				}
+			}
+			if (brackets > 0)//add one additional tab
+				newline += "\t";
+			
+			//The textarea value becomes: text before cursor + [enter] + an amount of tabs + text after cursor
+			target.value = value.substring(0, start) + "\n" + newline + value.substring(this.selectionEnd);
+			//put cursor after the old position + 1 enter + all tabs.
+			this.selectionStart = this.selectionEnd = start + newline.length + 1;
+			keystroke.preventDefault();
+			
+		} else if (keystroke.keyCode === 125) {// "}"
+	 
+			var start = this.selectionStart;
+			var target = keystroke.target;
+			var value = target.value;
 
-                        //Goes back to the beginning of the line and checks for tabs
-                        for (var i = textarea.selectionStart-1; i >= 0 ; i--) {
-                            if (value[i] == "\t") {//Tabs for each tab at the beginning of the previous line
-                                newline = newline + "\t";
-                            } else if (value[i] == "\n") {//Beginning of the line
-                                break;
-                            } else {//another character found: do not jump one tab back
-                                target.value = value.substring(0, start) + "}" + value.substring(this.selectionEnd);//add "}"
-                                this.selectionStart = this.selectionEnd = start + 1;
-                                keystroke.preventDefault();
-                                return;
-                            }
-                        }
-                        if (start == 0) {
-                            target.value = "}" + value.substring(this.selectionEnd);
-                            this.selectionStart = this.selectionEnd = start + 1;
-                        } else {
-                            //The textarea value becomes: text before cursor + 1 tab less + } + text after cursor
-                            target.value = value.substring(0, start-1) + "}" + value.substring(this.selectionEnd);
-                            //put cursor after the old position (-1 +1 character is no displacement).
-                            this.selectionStart = this.selectionEnd = start;
-                        }
-                        keystroke.preventDefault();
-                    }
-                },false);
-                document.querySelector("textarea").addEventListener('keydown',function(keystroke) {//keydown can detect tabs
-                     //Prevents an element outside of the textbox being selected whenever you press tab.
-                        if (keystroke.keyCode === 9) {//The keystroke was a tab
-                     
-                            var start = this.selectionStart;
-                            var target = keystroke.target;
-                            var value = target.value;
-                     
-                            //The textarea value becomes: text before cursor + tab + text after cursor
-                            target.value = value.substring(0, start) + "\t" + value.substring(this.selectionEnd);
-                            //put cursor at one after the old position (because of the added tab)
-                            this.selectionStart = this.selectionEnd = start + 1;
-                            keystroke.preventDefault();
-                        }
-                     },false);
-                </script>
+			//Goes back to the beginning of the line and checks for tabs
+			for (var i = textarea.selectionStart-1; i >= 0 ; i--) {
+				if (value[i] == "\t") {//Tabs for each tab at the beginning of the previous line
+					newline = newline + "\t";
+				} else if (value[i] == "\n") {//Beginning of the line
+					break;
+				} else {//another character found: do not jump one tab back
+					target.value = value.substring(0, start) + "}" + value.substring(this.selectionEnd);//add "}"
+					this.selectionStart = this.selectionEnd = start + 1;
+					keystroke.preventDefault();
+					return;
+				}
+			}
+			if (start == 0) {
+				target.value = "}" + value.substring(this.selectionEnd);
+				this.selectionStart = this.selectionEnd = start + 1;
+			} else {
+				//The textarea value becomes: text before cursor + 1 tab less + } + text after cursor
+				target.value = value.substring(0, start-1) + "}" + value.substring(this.selectionEnd);
+				//put cursor after the old position (-1 +1 character is no displacement).
+				this.selectionStart = this.selectionEnd = start;
+			}
+			keystroke.preventDefault();
+		}
+	},false);
+	document.querySelector("textarea").addEventListener('keydown',function(keystroke) {//keydown can detect tabs
+		 //Prevents an element outside of the textbox being selected whenever you press tab.
+			if (keystroke.keyCode === 9) {//The keystroke was a tab
+		 
+				var start = this.selectionStart;
+				var target = keystroke.target;
+				var value = target.value;
+		 
+				//The textarea value becomes: text before cursor + tab + text after cursor
+				target.value = value.substring(0, start) + "\t" + value.substring(this.selectionEnd);
+				//put cursor at one after the old position (because of the added tab)
+				this.selectionStart = this.selectionEnd = start + 1;
+				keystroke.preventDefault();
+			}
+		 },false);
+	</script>
 </body>
 </html>
